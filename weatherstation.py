7ad81660e3ffdbfd4434f1ff4ba6eac414b6cf0c
eosti <reidsox1@gmail.com>
2020-05-17 18:18:45 -0700

    fix printf error

--- weatherstation.py
+++ weatherstation.py
@@ -37,7 +37,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # Create temp/humidity object through I2C
 sensor = adafruit_si7021.SI7021(i2c)
 
-print('Connected as ' config.io_api_username)
+print('Connected as %s' % config.io_api_username)
 print('Reading sensors every %d seconds.' % LOOP_DELAY)
 
 while True:
