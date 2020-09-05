a2d834584c382ac37e7130024bbc3c374307a48d
eosti <reidsox1@gmail.com>
2020-09-04 23:56:12 -0700

    add encompassing exception handler

--- weatherstation.py
+++ weatherstation.py
@@ -53,93 +53,96 @@ i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_si7021.SI7021(i2c)
 
 logging.info('Reading sensors every %d seconds.' % LOOP_DELAY)
+try:
+    while True:
+        logging.debug('Reading sensors...')
 
-while True:
-    logging.debug('Reading sensors...')
-
-    # Read air quality
-    try:
-        uart.flushInput()       # Required to get the most current data
-        time.sleep(2)
-        data = uart.read(32)  # read up to 32 bytes
-    except Exception as e:
-        logging.warning('UART connection error. Skipping.')
-        logging.exception("Exception occurred")
+        # Read air quality
+        try:
+            uart.flushInput()       # Required to get the most current data
+            time.sleep(2)
+            data = uart.read(32)  # read up to 32 bytes
+        except Exception as e:
+            logging.warning('UART connection error. Skipping.')
+            logging.exception("Exception occurred")
 
 
-    data = list(data)
-    logging.debug("read: ", data)          # this is a bytearray type
+        data = list(data)
+        logging.debug("read: ", data)          # this is a bytearray type
 
-    buffer += data
+        buffer += data
 
-    while buffer and buffer[0] != 0x42:
-        buffer.pop(0)
+        while buffer and buffer[0] != 0x42:
+            buffer.pop(0)
 
-    if len(buffer) > 200:
-        buffer = []  # avoid an overrun if all bad data
-    if len(buffer) < 32:
-        continue
-
-    if buffer[1] != 0x4d:
-        buffer.pop(0)
-        continue
-
-    frame_len = struct.unpack(">H", bytes(buffer[2:4]))[0]
-    if frame_len != 28:
-        buffer = []
-        logging.warning('Incorrect UART packet length. Skipping.')
-        continue
-    try:
-        frame = struct.unpack(">HHHHHHHHHHHHHH", bytes(buffer[4:]))
-
-        pm10_standard, pm25_standard, pm100_standard, pm10_env, \
-            pm25_env, pm100_env, particles_03um, particles_05um, particles_10um, \
-            particles_25um, particles_50um, particles_100um, skip, checksum = frame
-
-        check = sum(buffer[0:30])
-
-        if check != checksum:
-            buffer = []
-            logging.warning('UART checksum error. Skipping.')
+        if len(buffer) > 200:
+            buffer = []  # avoid an overrun if all bad data
+        if len(buffer) < 32:
             continue
-    
-    except Exception as e:
-        logging.warning('Error in UART parsing.')
-        logging.exception("Exception occurred")
+
+        if buffer[1] != 0x4d:
+            buffer.pop(0)
+            continue
+
+        frame_len = struct.unpack(">H", bytes(buffer[2:4]))[0]
+        if frame_len != 28:
+            buffer = []
+            logging.warning('Incorrect UART packet length. Skipping.')
+            continue
+        try:
+            frame = struct.unpack(">HHHHHHHHHHHHHH", bytes(buffer[4:]))
+
+            pm10_standard, pm25_standard, pm100_standard, pm10_env, \
+                pm25_env, pm100_env, particles_03um, particles_05um, particles_10um, \
+                particles_25um, particles_50um, particles_100um, skip, checksum = frame
+
+            check = sum(buffer[0:30])
+
+            if check != checksum:
+                buffer = []
+                logging.warning('UART checksum error. Skipping.')
+                continue
+
+        except Exception as e:
+            logging.warning('Error in UART parsing.')
+            logging.exception("Exception occurred")
 
 
-    try:
-        # Read Si7021 
-        temp_data = sensor.temperature
-        humidity_data = sensor.relative_humidity
-    except Exception as e:
-        logging.warning('Si7021 read error. Skipping.')
-        logging.exception("Exception occurred")
+        try:
+            # Read Si7021 
+            temp_data = sensor.temperature
+            humidity_data = sensor.relative_humidity
+        except Exception as e:
+            logging.warning('Si7021 read error. Skipping.')
+            logging.exception("Exception occurred")
 
 
-    try:
-        # Data collected, let's send it in!
-        logging.debug('Sending data to Adafruit I/O...')
-        logging.debug("---------------------------------------")
+        try:
+            # Data collected, let's send it in!
+            logging.debug('Sending data to Adafruit I/O...')
+            logging.debug("---------------------------------------")
 
-        logging.debug('Temperature: %0.1f C' % temp_data)
-        aio.send(outside_temp_feed.key, temp_data)
-        logging.debug('Humidity: %0.1f %%' % humidity_data)
-        aio.send(outside_humidity_feed.key, humidity_data)
+            logging.debug('Temperature: %0.1f C' % temp_data)
+            aio.send(outside_temp_feed.key, temp_data)
+            logging.debug('Humidity: %0.1f %%' % humidity_data)
+            aio.send(outside_humidity_feed.key, humidity_data)
 
-        logging.debug('PM 1.0: ', pm10_env)
-        aio.send(pm_1_feed.key, pm10_env)
-        logging.debug('PM 2.5: ', pm25_env)
-        aio.send(pm_2_feed.key, pm25_env)
-        logging.debug('PM 10: ', pm100_env)
-        aio.send(pm_10_feed.key, pm100_env)
-    except Exception as e:
-        logging.error('Unable to upload data. Skipping.')
-        logging.exception("Exception occurred")
-    
-    # Reset buffer
-    buffer = buffer[32:]
+            logging.debug('PM 1.0: ', pm10_env)
+            aio.send(pm_1_feed.key, pm10_env)
+            logging.debug('PM 2.5: ', pm25_env)
+            aio.send(pm_2_feed.key, pm25_env)
+            logging.debug('PM 10: ', pm100_env)
+            aio.send(pm_10_feed.key, pm100_env)
+        except Exception as e:
+            logging.error('Unable to upload data. Skipping.')
+            logging.exception("Exception occurred")
 
-    # avoid timeout from adafruit io
-    time.sleep(LOOP_DELAY)
+        # Reset buffer
+        buffer = buffer[32:]
 
+        # avoid timeout from adafruit io
+        time.sleep(LOOP_DELAY)
+
+except Exception as e:
+    logging.critical('Something very bad just happened.')
+    logging.exception('Exception occurred')
