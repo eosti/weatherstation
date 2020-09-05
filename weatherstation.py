c844884b4f405eaadaef0f79bd1caa71b97a25c6
eosti <reidsox1@gmail.com>
2020-09-04 23:20:40 -0700

    initial logger implementation

--- weatherstation.py
+++ weatherstation.py
@@ -1,9 +1,10 @@
 try:
     import struct
-except ImportError:
+except Exception as e ImportError:
     import ustruct as struct
 
 import time
+import logging
 import board
 import busio
 import adafruit_si7021
@@ -16,7 +17,7 @@ from Adafruit_IO import Client, Feed, RequestError
 LOOP_DELAY = 120
 
 # Create an instance of the REST client
-print('Setting up Adafruit I/O and sensors...')
+logging.info('Setting up Adafruit I/O and sensors...')
 aio = Client(config.io_api_username, config.io_api_key)
 
 # Assign feeds
@@ -26,9 +27,9 @@ pm_1_feed = aio.feeds('pm-1-dot-0')
 pm_2_feed = aio.feeds('pm-2-dot-5')
 pm_10_feed = aio.feeds('pm-10')
 
-print('Connected as %s' % config.io_api_username)
+logging.info('Connected as %s' % config.io_api_username)
 
-print(datetime.now())
+logging.info(datetime.now())
 
 # Create UART object for air quality
 import serial
@@ -42,21 +43,32 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # Create temp/humidity object through I2C
 sensor = adafruit_si7021.SI7021(i2c)
 
-print('Reading sensors every %d seconds.' % LOOP_DELAY)
+# Logging stuff
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename='weatherstation.log'
+
+)
+
+logging.info('Reading sensors every %d seconds.' % LOOP_DELAY)
 
 while True:
-    # print('Reading sensors...')
+    logging.debug('Reading sensors...')
 
     # Read air quality
     try:
         uart.flushInput()       # Required to get the most current data
         time.sleep(2)
         data = uart.read(32)  # read up to 32 bytes
-    except:
-        print('UART connection error. Skipping.')
+    except Exception as e:
+        logging.warning('UART connection error. Skipping.')
+        logging.exception("Exception occurred")
+
 
     data = list(data)
-    # print("read: ", data)          # this is a bytearray type
+    logging.debug("read: ", data)          # this is a bytearray type
 
     buffer += data
 
@@ -75,7 +87,7 @@ while True:
     frame_len = struct.unpack(">H", bytes(buffer[2:4]))[0]
     if frame_len != 28:
         buffer = []
-        print('Incorrect UART packet length. Skipping.')
+        logging.warning('Incorrect UART packet length. Skipping.')
         continue
     try:
         frame = struct.unpack(">HHHHHHHHHHHHHH", bytes(buffer[4:]))
@@ -88,39 +100,44 @@ while True:
 
         if check != checksum:
             buffer = []
-            print('UART checksum error. Skipping.')
+            logging.warning('UART checksum error. Skipping.')
             continue
     
-    except:
-        print('Error in UART parsing.')
+    except Exception as e:
+        logging.warning('Error in UART parsing.')
+        logging.exception("Exception occurred")
+
 
     try:
         # Read Si7021 
         temp_data = sensor.temperature
         humidity_data = sensor.relative_humidity
-    except:
-        print('Si7021 read error. Skipping.')
+    except Exception as e:
+        logging.warning('Si7021 read error. Skipping.')
+        logging.exception("Exception occurred")
+
 
     try:
         # Data collected, let's send it in!
-        # print('Sending data to Adafruit I/O...')
-        # print("---------------------------------------")
+        logging.debug('Sending data to Adafruit I/O...')
+        logging.debug("---------------------------------------")
 
-        # print('Temperature: %0.1f C' % temp_data)
+        logging.debug('Temperature: %0.1f C' % temp_data)
         aio.send(outside_temp_feed.key, temp_data)
-        # print('Humidity: %0.1f %%' % humidity_data)
+        logging.debug('Humidity: %0.1f %%' % humidity_data)
         aio.send(outside_humidity_feed.key, humidity_data)
 
-        # print('PM 1.0: ', pm10_env)
+        logging.debug('PM 1.0: ', pm10_env)
         aio.send(pm_1_feed.key, pm10_env)
-        # print('PM 2.5: ', pm25_env)
+        logging.debug('PM 2.5: ', pm25_env)
         aio.send(pm_2_feed.key, pm25_env)
-        # print('PM 10: ', pm100_env)
+        logging.debug('PM 10: ', pm100_env)
         aio.send(pm_10_feed.key, pm100_env)
-    except:
-        print('Unable to upload data. Skipping.')
+    except Exception as e:
+        logging.error('Unable to upload data. Skipping.')
+        logging.exception("Exception occurred")
     
-    # print()
+    logging.debug()
 
     # Reset buffer
     buffer = buffer[32:]
