44d960eab2e1d94ebc0157a994654b565b9ca009
eosti <reidsox1@gmail.com>
2021-01-03 18:45:58 -0800

    migrate to adafruit-circuitpython-pm25

--- weatherstation.py
+++ weatherstation.py
@@ -4,19 +4,13 @@ try:
 except Exception as e:
     import ustruct as struct
 
-import time
 import logging
 import logging.handlers as handlers
 import board
 import busio
 import adafruit_si7021
-from datetime import datetime
+from adafruit_pm25.uart import PM25_UART
 import aqi
-import signal
-from importlib import reload
-import os
-import sys
-
 import config
 from Adafruit_IO import Client, Feed, RequestError
 
@@ -50,117 +44,60 @@ aqi_feed = aio.feeds('aqi')
 
 logger.info('Connected as %s' % config.io_api_username)
 
-# Create UART object for air quality
-import serial
-uart = serial.Serial()
-uart.baudrate = 9600
-uart.port = "/dev/ttyS0"
-uart.timeout = 0.25
+# Create UART PM2.5 device
+uart = busio.UART("/dev/ttyS0", baudrate=9600, timeout=0.25)
+reset_pin = None
+pm25 = PM25_UART(uart, reset_pin)
 
-# Buffer for UART
-buffer = []
-
-# Create I2C object
+# Create I2C SI7021 device
 i2c = busio.I2C(board.SCL, board.SDA)
-# Create temp/humidity object through I2C
 sensor = adafruit_si7021.SI7021(i2c)
 
-# timeout handlers
-def signal_handler(signum, frame):
-    raise Exception("Timed out!")
-signal.signal(signal.SIGALRM, signal_handler)
+logger.debug('Reading sensors...')
 
+# Read air quality
 try:
-    logger.debug('Reading sensors...')
-
-    # Read air quality
-    try:
-        uart.open();
-        data = uart.read(32)  # read up to 32 bytes
-        uart.close();
-    except Exception as e:
-        logger.warning('UART connection error. Skipping.')
-        logger.exception("Exception occurred")
-
-    data = list(data)
-    buffer += data
-
-    while buffer and buffer[0] != 0x42:
-        buffer.pop(0)
-
-    if len(buffer) > 200:
-        buffer = []  # avoid an overrun if all bad data
-
-    if buffer[1] != 0x4d:
-        buffer.pop(0)
-
-    frame_len = struct.unpack(">H", bytes(buffer[2:4]))[0]
-    if frame_len != 28:
-        buffer = []
-        logger.warning('Incorrect UART packet length. Skipping.')
-
-    try:
-        frame = struct.unpack(">HHHHHHHHHHHHHH", bytes(buffer[4:]))
-
-        pm10_standard, pm25_standard, pm100_standard, pm10_env, \
-            pm25_env, pm100_env, particles_03um, particles_05um, particles_10um, \
-            particles_25um, particles_50um, particles_100um, skip, checksum = frame
-
-        check = sum(buffer[0:30])
-
-        if check != checksum:
-            buffer = []
-            logger.warning('UART checksum error. Skipping.')
-
-    except Exception as e:
-        logger.warning('Error in UART parsing.')
-        logger.exception("Exception occurred")
-
-    # Convert PM values to AQI
-    try:
-        current_aqi = aqi.to_aqi([
-            (aqi.POLLUTANT_PM25, pm25_env),
-            (aqi.POLLUTANT_PM10, pm100_env),
-        ])
-    except Exception as e:
-        logger.warning('PM conversion failed. Skipping.')
-        logger.exception("Exception occurred")
-
-    # Read Si7021 
-    try:
-        temp_data = sensor.temperature
-        humidity_data = sensor.relative_humidity
-    except Exception as e:
-        logger.warning('Si7021 read error. Reloading library. Skipping.')
-        logger.exception("Exception occurred")
-        adafruit_si7021 = reload(adafruit_si7021) # Reloads sensor library
-        sensor = adafruit_si7021.SI7021(i2c)
-
-    # Data collected, let's send it in!
-    try:
-        logger.debug('Sending data to Adafruit I/O...')
-        logger.debug("---------------------------------------")
-
-        logger.debug('Temperature: %0.1f C' % temp_data)
-        aio.send(outside_temp_feed.key, temp_data)
-        logger.debug('Humidity: %0.1f %%' % humidity_data)
-        aio.send(outside_humidity_feed.key, humidity_data)
-
-        logger.debug('PM 1.0: %0i' % pm10_env)
-        aio.send(pm_1_feed.key, pm10_env)
-        logger.debug('PM 2.5: %0i' % pm25_env)
-        aio.send(pm_2_feed.key, pm25_env)
-        logger.debug('PM 10: %0i' % pm100_env)
-        aio.send(pm_10_feed.key, pm100_env)
-        logger.debug('AQI: %0i' % float(current_aqi))
-        aio.send(aqi_feed.key, float(current_aqi))
-    except Exception as e:
-        logger.error('Unable to upload data. Skipping.')
-        logger.exception("Exception occurred")
-
-    # Reset buffer
-    buffer = buffer[32:]
-
+    aqdata = pm25.read()
 except Exception as e:
-    logger.error('Something went wrong!')
+    logger.warning('Unable to read air quality data. Skipping.')
+    logger.exception("Exception occurred")
+
+# Convert PM values to AQI
+try:
+    current_aqi = aqi.to_aqi([
+        (aqi.POLLUTANT_PM25, aqdata["pm25 env"]),
+        (aqi.POLLUTANT_PM10, aqdata["pm100 env"]),
+    ])
+except Exception as e:
+    logger.warning('PM conversion failed. Skipping.')
+    logger.exception("Exception occurred")
+
+# Read Si7021 
+try:
+    temp_data = sensor.temperature
+    humidity_data = sensor.relative_humidity
+except Exception as e:
+    logger.warning('Si7021 read error. Reloading library. Skipping.')
+    logger.exception("Exception occurred")
+
+# Data collected, let's send it in!
+try:
+    logger.debug('Sending data to Adafruit I/O...')
+    logger.debug("---------------------------------------")
+
+    logger.debug('Temperature: %0.1f C' % temp_data)
+    aio.send(outside_temp_feed.key, temp_data)
+    logger.debug('Humidity: %0.1f %%' % humidity_data)
+    aio.send(outside_humidity_feed.key, humidity_data)
+
+    logger.debug('PM 1.0: %0i' % aqdata["pm10 env"])
+    aio.send(pm_1_feed.key, aqdata["pm10 env"])
+    logger.debug('PM 2.5: %0i' % aqdata["pm25 env"])
+    aio.send(pm_2_feed.key, aqdata["pm25 env"])
+    logger.debug('PM 10: %0i' % aqdata["pm100 env"])
+    aio.send(pm_10_feed.key, aqdata["pm100 env"])
+    logger.debug('AQI: %0i' % float(current_aqi))
+    aio.send(aqi_feed.key, float(current_aqi))
+except Exception as e:
+    logger.error('Unable to upload data. Skipping.')
     logger.exception("Exception occurred")
