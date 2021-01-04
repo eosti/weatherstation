2368c3dcf1f455f856d5494ad8189955f135bd13
eosti <reidsox1@gmail.com>
2021-01-03 19:06:37 -0800

    update sensor example

--- test_sensors/airquality_example.py
+++ test_sensors/airquality_example.py
@@ -1,76 +1,50 @@
-try:
-    import struct
-except ImportError:
-    import ustruct as struct
+"""
+Example sketch to connect to PM2.5 sensor with either I2C or UART.
+Example modified from https://github.com/adafruit/Adafruit_CircuitPython_PM25
+"""
 
-# Connect the sensor TX pin to the board/computer RX pin
-# For use with a microcontroller board:
-# import board
-# import busio
-# uart = busio.UART(board.TX, board.RX, baudrate=9600)
+import time
+import board
+import busio
+from adafruit_pm25.uart import PM25_UART
 
-# For use with Raspberry Pi/Linux:
+
+reset_pin = None
 import serial
-uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
+uar = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
 
-# For use with USB-to-serial cable:
-# import serial
-# uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=0.25)
+# Connect to a PM2.5 sensor over UART
+pm25 = PM25_UART(uart, reset_pin)
 
-buffer = []
+print("Found PM2.5 sensor, reading data...")
 
 while True:
-    data = uart.read(32)  # read up to 32 bytes
-    data = list(data)
-    # print("read: ", data)          # this is a bytearray type
+    time.sleep(1)
 
-    buffer += data
-
-    while buffer and buffer[0] != 0x42:
-        buffer.pop(0)
-
-    if len(buffer) > 200:
-        buffer = []  # avoid an overrun if all bad data
-    if len(buffer) < 32:
+    try:
+        aqdata = pm25.read()
+    except RuntimeError:
+        print("Unable to read from sensor, retrying...")
         continue
 
-    if buffer[1] != 0x4d:
-        buffer.pop(0)
-        continue
-
-    frame_len = struct.unpack(">H", bytes(buffer[2:4]))[0]
-    if frame_len != 28:
-        buffer = []
-        continue
-
-    frame = struct.unpack(">HHHHHHHHHHHHHH", bytes(buffer[4:]))
-
-    pm10_standard, pm25_standard, pm100_standard, pm10_env, \
-        pm25_env, pm100_env, particles_03um, particles_05um, particles_10um, \
-        particles_25um, particles_50um, particles_100um, skip, checksum = frame
-
-    check = sum(buffer[0:30])
-
-    if check != checksum:
-        buffer = []
-        continue
-
+    print()
     print("Concentration Units (standard)")
     print("---------------------------------------")
-    print("PM 1.0: %d\tPM2.5: %d\tPM10: %d" %
-          (pm10_standard, pm25_standard, pm100_standard))
+    print(
+        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+        % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
+    )
     print("Concentration Units (environmental)")
     print("---------------------------------------")
-    print("PM 1.0: %d\tPM2.5: %d\tPM10: %d" % (pm10_env, pm25_env, pm100_env))
+    print(
+        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+        % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
+    )
     print("---------------------------------------")
-    print("Particles > 0.3um / 0.1L air:", particles_03um)
-    print("Particles > 0.5um / 0.1L air:", particles_05um)
-    print("Particles > 1.0um / 0.1L air:", particles_10um)
-    print("Particles > 2.5um / 0.1L air:", particles_25um)
-    print("Particles > 5.0um / 0.1L air:", particles_50um)
-    print("Particles > 10 um / 0.1L air:", particles_100um)
+    print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
+    print("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
+    print("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
+    print("Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
+    print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
+    print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
     print("---------------------------------------")
-
-    buffer = buffer[32:]
-    # print("Buffer ", buffer)
-
